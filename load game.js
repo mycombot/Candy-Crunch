b0437da1eabc6f42c8824e5bd7df7fdca6c8a47e
David D. Roberts <robertsdavidddr@gmail.com>
2013-02-01 23:52:04 -0800

    Cleaned up "load game.js" a bit.

--- load game.js
+++ load game.js
@@ -1,25 +1,22 @@
-"use strict";
+/*global console createjs mainWindow linkHTMLDisplayTo $ _ jQuery*/
 var startGame = function() {
+	"use strict";
+	
 	var options = {
 		preferFlash: false,
 		EaselJS_url: 'http://code.createjs.com/easeljs-0.5.0.min.js',
 		EaselFL_url: '../EaselFL/build/output/easelfl-cur.min.js',
 		SWFObject_url: '../EaselFL/js/swfobject.js',
-	}
+	};
 	createjs.FLSetup.run(onSetupSuccess, onSetupFailure, options);
-	
-	function onSetupFailure() {
-		console.error('EaselFL failed to load; aborting.');
-	}
 
 	function onSetupSuccess(isFL){
 		var loadOtherScriptsCount = 0;
-		var loadOtherScripts = function() {
-			var jsAr = new Array();
-			jsAr[0] = "../tween.js/lib/tweenjs-0.3.0.min.js";
-			jsAr[1] = "../sound.js/lib/soundjs-0.3.0.min.js";
-			jsAr[2] = "../easel.js/src/easeljs/filters/ColorFilter.js";
-			
+		var jsAr = [
+			"../tween.js/lib/tweenjs-0.3.0.min.js",
+			"../sound.js/lib/soundjs-0.3.0.min.js",
+			"../easel.js/src/easeljs/filters/ColorFilter.js" ];
+		
 		var onAllLoaded = function() {
 			if(loadOtherScriptsCount === jsAr.length) {
 				console.log('All scripts loaded.');
@@ -28,27 +25,24 @@ var startGame = function() {
 			}
 		};
 		
-		for (var i = 0; i < jsAr.length; i++) {
-				var js = jsAr[i]; 
-				console.log('getting ' + js);
-				$.ajax({
-					async: false,
-					type: "GET",
-					url: js,
-					dataType: 'text',
+		jsAr.map(function(js) {
+			console.log('getting ' + js);
+			$.ajax({
+				async: false,
+				type: "GET",
+				url: js,
+				dataType: 'text',
 
-					success: function(data){
-						jQuery.globalEval(data);
-						loadOtherScriptsCount += 1;
-						onAllLoaded();
-					},
-					
-					error: function(data) {
-						console.log('An error occured loading a script via AJAX.');
-					}
-					
-				});
-			}
-		}();
+				success: function(data){
+					jQuery.globalEval(data);
+					loadOtherScriptsCount += 1;
+					onAllLoaded();
+				},
+				error: function() {console.log('An error occured loading a script via AJAX.');}
+				
+			});
+		});
 	}
-}
+	
+	function onSetupFailure() {console.error('EaselFL failed to load; aborting.');}
+};
